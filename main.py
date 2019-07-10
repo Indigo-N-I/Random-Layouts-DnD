119ca2b64e0872119ab8a09a6a9a20415a7f8808
Tim Huang <readatontim@gmail.com>
2019-07-09 21:58:34 -0700

    Created opening gui

---
+++ main.py
@@ -0,0 +1,57 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+)
+from kivy.vector import Vector
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
+import re
+
+class IntInput(TextInput):
+
+    pat = re.compile('[0-9]')
+    def insert_text(self, substring, from_undo=False):
+        pat = self.pat
+        if pat.match(substring):
+            return super(IntInput, self).insert_text(substring, from_undo = from_undo)
+        else:
+            return super(IntInput, self).insert_text('', from_undo = from_undo)
+
+
+class SizeInput(TextInput):
+
+    def insert_text(self, substring, from_undo=False):
+        if len(self.text) == 1:
+            return super(SizeInput, self).insert_text('', from_undo = from_undo)
+        elif (substring == 'S' or substring == 'M' or substring == 'L'):
+            return super(SizeInput, self).insert_text(substring, from_undo = from_undo)
+        else:
+            return super(SizeInput, self).insert_text('', from_undo = from_undo)
+
+class OptionsScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(OptionsScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='Size of Map (S, M, L)'))
+        self.defSize = SizeInput()
+        self.add_widget(self.defSize)
+        self.add_widget(Label(text='Number of Counters'))
+        self.counters = IntInput()
+        self.add_widget(self.counters)
+        self.add_widget(Button(text = 'Build'))
+        self.add_widget(Button(text = 'Advanced Properties'))
+
+
+class MyApp(App):
+
+    def build(self):
+        return OptionsScreen()
+
+
+if __name__ == '__main__':
+    MyApp().run()
